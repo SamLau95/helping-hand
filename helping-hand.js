00112805c37dfe334d452dfc54599dc2111b9592
Charles Xue <charlesx2013@gmail.com>
2014-03-08 21:25:56 -0800

    Started result template, fixed query bug

--- helping-hand.js
+++ helping-hand.js
@@ -1,10 +1,22 @@
-var query = "";
+
 var NonProfits = new Meteor.Collection('nonprofits');
 
+function isEmpty(str) {
+  return (!str || 0 === str.length);
+}
+
 if (Meteor.isClient) {
+  Session.set("query", "");
+  Session.set("emptyQuery", true);
   Template.searchForm.events({
-    'keypress input': function(e, t) {
+    'keyup input': function(e, t) {
       query = t.find('#search').value;
+      Session.set("query", query);
+      Session.set("emptyQuery", isEmpty(query));
     }
   });
+
+  Template.results.noSearch = function() {
+    return Session.get("emptyQuery");
+  }
 }
