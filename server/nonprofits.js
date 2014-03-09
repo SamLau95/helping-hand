11500897035e482cc7ac143b70d3bb89a8a40b25
Sam Lau <samlau95@gmail.com>
2014-03-08 22:24:53 -0800

    Nonprofits seeds

---
+++ server/nonprofits.js
@@ -0,0 +1,11 @@
+Nonprofits = new Meteor.Collection('nonprofits');
+
+Meteor.startup(function() {
+  if (Nonprofits.find().count() == 0) {
+    Nonprofits.insert({ name: 'Common Change', keywords: ['funding'] });
+    Nonprofits.insert({ name: 'Partners in School Innovation',
+                        keywords: ['education', 'literacy']});
+    Nonprofits.insert({ name: 'IISME',
+                        keywords: ['STEM', 'education', 'teachers'] });
+  }
+});
